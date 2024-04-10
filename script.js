28eafa181df914e9667c267bb2d6fe3d7a2e0f6f
James Steed <james.steed23@student.cart.org>
2024-04-10 10:36:31 -0700

    changed functions to not remove text

--- script.js
+++ script.js
@@ -17,25 +17,25 @@ class Player {
 
 var NPC = {
     business: {
-        dialogue: ["I have SO much work to do. I literally have three coloring pages due this afternoon. I can't.", 
-        "What do you mean you only have one formal dress day?", 
-        "Do you think you could code a site about Elon Musk for me?", 
-        "I have to get second opinions on my project about big business' effect on the “environment crisis”. First it was global cooling, then global warming, then global cooling, then global warming AGAIN? It can't be real.", 
-        "I'm gonna be honest I don't think homelessness is a problem if I can make money off it.",
-        "DME kids are SO stuck up. What do you mean you're from the tech labs? What's a “web application”?",
-        "Business is my passion.",
-        "Hey bro."],
+        dialogue: ["I have SO much work to do. I literally have three coloring pages due this afternoon. I can't.",
+            "What do you mean you only have one formal dress day?",
+            "Do you think you could code a site about Elon Musk for me?",
+            "I have to get second opinions on my project about big business' effect on the “environment crisis”. First it was global cooling, then global warming, then global cooling, then global warming AGAIN? It can't be real.",
+            "I'm gonna be honest I don't think homelessness is a problem if I can make money off it.",
+            "DME kids are SO stuck up. What do you mean you're from the tech labs? What's a “web application”?",
+            "Business is my passion.",
+            "Hey bro."],
         buttons: ["OK.",
             "I'm busy.",
-        "Isn't crypto cool?",
-        "How's your stocks?",
-        "Bitcoin is up by 0.1%.",
-        "Tesla went bankrupt.",
-        "Leave me alone.",
-        "You're obsessed with being rich.",
-        "I only invest in crypto.",
-        "(Silence).",
-        "Doge is the only true currency."],
+            "Isn't crypto cool?",
+            "How's your stocks?",
+            "Bitcoin is up by 0.1%.",
+            "Tesla went bankrupt.",
+            "Leave me alone.",
+            "You're obsessed with being rich.",
+            "I only invest in crypto.",
+            "(Silence).",
+            "Doge is the only true currency."],
         atk: [1],
         drop: [1],
     }
@@ -98,7 +98,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         hidePage("load-page");
         removeHidden("home-page");
-        setText(student.name);
+        document.getElementById("name-container").innerText = student.name;
 
 
     });
@@ -108,7 +108,6 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("locIcon").addEventListener("click", function () {
 
-        setText("");
         setBackground = ("")
 
     });
@@ -118,7 +117,7 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("backIcon").addEventListener("click", function () {
 
-        setText("");
+        setBackground("");
 
     });
 });
